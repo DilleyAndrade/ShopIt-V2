ef82230fffd908a6612d0eb8fd613ac24fe34a5b
Dilley Andrade <dilleyandrade@gmail.com>
2024-09-18 19:13:51 -0300

    feat: created production and development error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -4,8 +4,17 @@ export default (err, req, res, next) => {
     message: err?.message || "Internal server error"
   }
 
-  res.status(error.statusCode).json({
-    message: error.message,
-    
-  })
+  if(process.env.NODE_ENV === 'DEVELOPMENT') {
+    res.status(error.statusCode).json({
+      message: error.message,
+      error: err,
+      stack: err?.stack
+    })
+  }
+
+  if(process.env.NODE_ENV === 'PRODUCTION') {
+    res.status(error.statusCode).json({
+      message: error.message
+    })
+  }
 }
