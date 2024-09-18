bd8b4baf6a586d5843ab47753fd9b0c994f60032
Dilley Andrade <dilleyandrade@gmail.com>
2024-09-18 20:22:24 -0300

    feat: added validation and mongoose id error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -1,7 +1,21 @@
+import ErrorHandler from '../utils/errorHandler.js'
+
 export default (err, req, res, next) => {
   let error = {
     statusCode: err?.statusCode || 500,
     message: err?.message || "Internal server error"
+  }
+
+  //Handle Invalid Mongoose ID Error
+  if(err.name === 'CastError') {
+    const message = `Resource not found. Invalid: ${err?.path}`
+    error = new ErrorHandler(message, 404)
+  }
+
+  //Handle Validation Error Quando esquecer de adicionar algo na hora de criar, product name, propduct price e etc
+  if(err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((value) => value.message)
+    error = new ErrorHandler(message, 404)
   }
 
   if(process.env.NODE_ENV === 'DEVELOPMENT') {
